c5f2e9b81f817c16c63877962c74d14ee649a08c
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-25 12:53:18 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -3,12 +3,24 @@ from pyrogram import Client, filters, emoji
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from utils import get_search_results
 from info import MAX_RESULTS, CACHE_TIME, SHARE_BUTTON_TEXT
-
+from info import AUTH_CHANEL
 
 @Client.on_inline_query()
 async def answer(_, i_query: InlineQuery):
     """Show search results for given inline query"""
-
+    try:
+        await _.get_chat_member(
+          chat_id=AUTH_CHANEL,
+          user_id=i_query.from_user.id
+        )
+    except pyrogram.errors.exceptions.bad_request_400.UserNotParticipant:
+        await i_query.answer(
+            results = [],
+            cache_time=CACHE_TIME,
+            switch_pm_text="You must Join my Group to use me",
+            switch_pm_parameter="okay"
+        )
+        return
     results = []
     if '|' in i_query.query:
         string, file_type = i_query.query.split('|', maxsplit=1)
