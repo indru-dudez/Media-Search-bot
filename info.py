938dc37a9464a75d88c3359adc123fcba86ca864
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-25 12:40:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,9 +23,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I'm Movie Search bot**
 
-Here you can search files in inline mode. Just press following buttons and start searching.
+Here you can search movies in inline mode. Just press following buttons and start searching.
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching movies'
